5311823078306f98165a35227e2e20911988eef6
Muhammed Berisso <muhammedb8826@gmail.com>
2025-08-18 04:45:49 +0300

    Add auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { KeycloakAuthStrategy } from './keycloak/auth/auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [
-    AppService,
-  ],
+  imports: [PassportModule],
+  controllers: [AppController, UsersController, AuthController],
+  providers: [AppService, KeycloakAuthStrategy, AuthService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Complain & Appeal API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+    customSiteTitle: 'Complain & Appeal API Docs',
+  });
+  app.getHttpAdapter().get('/api', (_req, res) => res.redirect('/api/docs'));
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
